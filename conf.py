72719ba9908ea514f5d237cb111aeb6a83753f17
Guillaume Baty <guillaume.baty@inria.fr>
2014-07-01 15:38:36 +0200

    Updated website (automated commit)

--- conf.py
+++ conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.inheritance_diagram',
+    'openalea.misc.dataflow_directive',
+    'openalea.misc.sphinx_role',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -123,7 +125,7 @@ html_theme_path = ['themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = 'OpenAlea community website'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
